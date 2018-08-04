05319272b3d08fce743050f1bb411c861725c1a1
kivancekici <kivancekici@gmail.com>
2018-08-04 17:50:48 +0300

    dfdf

--- js/main.js
+++ js/main.js
@@ -5,6 +5,18 @@ firebase.auth().signInAnonymously().catch(function (error) {
     // ...
 });
 
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      // ...
+    } else {
+      // User is signed out.
+      // ...
+    }
+    // ...
+  });
 
 function kaydetVeritabani(tablo, id, veri) {
 
